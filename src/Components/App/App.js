fc6c05af89663085969afa35d273294d77cbfbd1
Michael <mschneider247@gmail.com>
2020-07-25 13:58:32 -0600

    Link Component with App, add welcome message

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
+import Welcome from '../Welcome/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Welcome!</h1>
-        <h4>Are you ready for a game of Jeopardy?!</h4>
-        <h5>invite your friends!</h5>
-      </header>
+      <Welcome />
       <section>
         Player name input
       </section>
